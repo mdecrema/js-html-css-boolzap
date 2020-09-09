14ed9106ff9197a3e58d90aa052b7fa931c81aa8
marco <marcodecrema@libero.it>
2020-09-09 20:58:17 +0200

    funzione di risposta

--- js/script.js
+++ js/script.js
@@ -1,27 +1,48 @@
 $(document).ready(function() {
   var btnMex = $(".btn-mex");
-
+  // Click
   btnMex.click(function() {
     inviaMessaggio();
   });
-
-  $(".txt-mex").keypress(function(event) {
-    console.log(event.keyCode);
-    if (event.which == 13) {
-      inviaMessaggio();
-    }
+  // Invio
+  $(".txt-mex").keypress(function() {
+  tastoInvio();
   });
 
-  function inviaMessaggio() {
-    var templateMex = $(".template .messaggi").clone();
-    var txtMex = $(".txt-mex").val();
-    if (txtMex != "") {
-      templateMex.find(".text").text(txtMex);
-      templateMex.addClass("right");
-      $(".elenco_messaggi").append(templateMex);
+  function tastoInvio() {
+    var check = false;
+    if (event.which == 13) {
+      inviaMessaggio();
+      check = true;
     } else {
-      console.log("err");
+      check = false;
     }
+    return check;
   };
+
+  // Funzione che invia il messaggio
+  function inviaMessaggio() {
+      var templateMex = $(".template .messaggi").clone();
+      var txtMex = $(".txt-mex").val();
+      if (txtMex != "") {
+        templateMex.find(".text").text(txtMex);
+        templateMex.addClass("right");
+        $(".elenco_messaggi").append(templateMex);
+        $(".txt-mex").val("");
+        setTimeout(function() {
+          risposta();
+        }, 1000);
+      } else {
+        console.log("err");
+      }
+  };
+
+  // Funzione di risposta
+  function risposta() {
+    var templateMex = $(".template .messaggi").clone();
+    templateMex.find(".text").text("ok");
+    $(".elenco_messaggi").append(templateMex);
+  };
+
 
 });
