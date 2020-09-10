d8182fc09c2359624f3514c85043dc38429856d6
marco <marcodecrema@libero.it>
2020-09-10 12:12:03 +0200

    funzione ricerca contatti

--- js/script.js
+++ js/script.js
@@ -50,25 +50,32 @@ $(document).ready(function() {
     var d = new Date();
     var ora = d.getHours();
     var minuti = d.getMinutes();
+    if (minuti < 10) {
+      minuti = "0"+minuti;
+    }
     var orario = ora+":"+minuti;
     return orario;
   };
 
-  $(".lente").click(function() {
-    trovaContatto();
-  });
 
-  function trovaContatto() {
-    var lettere = $(".txt-cerca").val();
-    var elencoContatti = ["Michele", "Fabio", "Samuele", "Alessandro B.", "Alessandro L.", "Claudia", "Davide", "Federico"];
-    for (i=1; i<=elencoContatti.length; i++) {
-      if (elencoContatti[i].includes(lettere)) {
-        $(this).addClass("hidden");
+  // Funzione di ricerca contatti 
+  $(".txt-cerca").keyup(function() {
+    // Prende come valore le lettere passate nell'input
+    var lettere = $(this).val().toLowerCase();
+    var contatti = $(".nome_contatto h5");
+    // 'Each' esegue un ciclo sui vari contatti
+    contatti.each(function() {
+      var nome = $(this).text().toLowerCase();
+      // Se i contatti analizzati contengono le lettere passate
+      if (nome.includes(lettere) == true) {
+        // Allora rimaranno visibili
+        $(this).parents(".prev_contatto").show();
       } else {
-        console.log("err");
+        // Altrimenti veranno nascosti
+        $(this).parents(".prev_contatto").hide();
       }
-    }
-  };
+    });
+  });
 
 
 });
